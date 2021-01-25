c5d34663728c807cc1c2b93dd7601e68820680d2
Anthony Quinn <58011993+AnthonyQ98@users.noreply.github.com>
2021-01-25 09:40:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+"""
+Anthony Quinn X00138635
+Lab 1
+25 Jan 2021
+"""
+
+# EXERCISE ONE
+"""
+total_rainfall = 0
+week_rainfall = []
+
+for number in range(7):
+    rainfall = float(input("Rainfall (in cm): "))
+    week_rainfall.append(rainfall)
+    total_rainfall += rainfall
+
+
+for i in range(len(week_rainfall)):
+    if week_rainfall[i] > 3.5:
+        print("Rainfall exceeded 3.5cm on Day" + str(i + 1))
+
+average_rainfall = total_rainfall / 7
+
+print(total_rainfall)
+print(round(average_rainfall, 2))
+"""
+
+numbers = []
+
+for i in range(5):
+    number = float(input("Number: "))
+    numbers.append(number)
+print(numbers)
+
+for x in range(len(numbers)):
+    numbers[x] = numbers[x] + 1
+print(numbers)
+
+
+
